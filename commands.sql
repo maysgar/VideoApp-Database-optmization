cb435432d9016d8b69670b9610396cd7381462e2
maysgar <100346215@alumnos.uc3m.es>
2017-05-05 00:46:34 +0200

    apuntados execution results premoh

--- commands.sql
+++ commands.sql
@@ -8,3 +8,25 @@ exec pkg_costes.run_test;
 --run the function:
 bill('99/48764198/49T', '11', '2016' ,'Premium Rider'); --'Alba','Perez'
 
+
+--RESULTS OF THE EXECUTION:
+/*Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+Contratos facturados en este mes:  4758
+RESULTS AT 05-MAY-17
+TIME CONSUMPTION: 158656 milliseconds.
+CONSISTENT GETS: 192096 blocks
+
+PL/SQL procedure successfully completed.
+
+Elapsed: 00:07:19.25*/
+
+
